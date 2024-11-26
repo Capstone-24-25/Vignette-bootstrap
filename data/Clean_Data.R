aa9cba34101b9fb2ba6140de92d8f592515eab1c
su029 <sumengxu@ucsb.edu>
2024-11-26 14:05:12 -0800

    add to bootstrap basic codes

--- data/Clean_Data.R
+++ data/Clean_Data.R
@@ -3,6 +3,6 @@ library(dplyr)
 Obesity_Raw <- read_csv("data/Obesity_Raw.csv")
 View(Obesity_Raw)
 
-Obesity_sample <- Obesity_Raw %>% sample_n(500)
+Obesity_sample <- Obesity_Raw %>% sample_n(700)
 
 save(Obesity_sample, file = "data/Obesity_sample.RData")
